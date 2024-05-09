4c15623fbef74958bdb26c39fcc3a18459fa05e1
kimpossible76 <167747819+kimpossible76@users.noreply.github.com>
2024-05-09 19:39:11 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,9 @@ from tqdm import tqdm
 import os
 import torch.nn as nn
 from const import *
-
+import torch.backends.cudnn as cudnn
+import DataSet as myDataLoader
+import torch.optim.lr_scheduler
 
 LOGGING_NAME="custom"
 def set_logging(name=LOGGING_NAME, verbose=True):
@@ -185,3 +187,39 @@ def save_checkpoint(state, filenameCheckpoint='checkpoint.pth.tar'):
 
 def netParams(model):
     return np.sum([np.prod(parameter.size()) for parameter in model.parameters()])
+
+def validation(model,):
+    '''
+    Main function for trainign and validation
+    :param args: global arguments
+    :return: None
+    '''
+
+    # load the model
+    # model = net.TwinLiteNet()
+    cuda_available = torch.cuda.is_available()
+    if cuda_available:
+        # model = torch.nn.DataParallel(model)
+        model = model.cuda()
+        cudnn.benchmark = True
+        
+
+    valLoader = torch.utils.data.DataLoader(
+        myDataLoader.colabIADDataset(valid=True),
+        batch_size=16, shuffle=False, num_workers=0, pin_memory=True)
+
+    total_paramters = netParams(model)
+    print('Total network parameters: ' + str(total_paramters))
+    
+    # model.load_state_dict(torch.load(args.weight))
+    model.eval()
+    # example = torch.rand(1, 3, 360, 640).cuda()
+    # model = torch.jit.trace(model, example)
+    da_segment_results,ll_segment_results = val(valLoader, model)
+
+    msg =  'Driving area Segment: Acc({da_seg_acc:.3f})    IOU ({da_seg_iou:.3f})    mIOU({da_seg_miou:.3f})\n' \
+                      'Lane line Segment: Acc({ll_seg_acc:.3f})    IOU ({ll_seg_iou:.3f})  mIOU({ll_seg_miou:.3f})'.format(
+                          da_seg_acc=da_segment_results[0],da_seg_iou=da_segment_results[1],da_seg_miou=da_segment_results[2],
+                          ll_seg_acc=ll_segment_results[0],ll_seg_iou=ll_segment_results[1],ll_seg_miou=ll_segment_results[2])
+    print(msg)
+
